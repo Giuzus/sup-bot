4508a8f284415057f003f5670845efd5e3006135
Gui Rangel <guirangel.310@gmail.com>
2018-05-07 21:48:27 -0300

    Comando de fila e customização dos outputs

--- src/default-config.js
+++ src/default-config.js
@@ -6,7 +6,7 @@ module.exports = function (bot, config) {
 
     bot.config = new __.lib.Config({
         command: new __.lib.Config({
-            symbol: '!'
+            symbol: '-'
         }),
         discord: new __.lib.Config({
             token: configJson.discord.token,
--- src/modules/queue.js
+++ src/modules/queue.js
@@ -20,7 +20,10 @@ module.exports = function (bot) {
             leaveQueue(msg.member, msg.channel);
         },
         next: (msg) => {
-            queueNext(msg.channel);
+            queueNext(msg.member, msg.channel);
+        },
+        queue: (msg) => {
+            listQueue(msg.channel);
         }
     }
 
@@ -33,9 +36,6 @@ module.exports = function (bot) {
 }
 
 //TODO:
-//Tratar erro de permissão ao mover usuario de canal.
-//Remover usuarios desconectados/deslogados.
-//comando para listar usuarios na fila.
 //comando para listar canais configurados.
 
 //não sei a melhor forma de estruturar, então vai ficar aqui mesmo, sinta-se livre para organizar
@@ -44,35 +44,33 @@ let active;
 let playChannel;
 let waitChannel;
 
-function isMemberInQueue(member)
-{
+function isMemberInQueue(member) {
     return queue.find(x => x.id === member.id) != null;
 }
 
 function joinQueue(member, textChannel) {
 
-    if(member.voiceChannel == null)
-    {
-        textChannel.send(`Você precisa estar em um canal de voz para entrar na fila (${member.displayName})`);
+    if (member.voiceChannel == null) {
+        textChannel.send(`**Você precisa estar em um canal de voz para entrar na fila (${member.displayName})** :face_palm:`);
         return;
     }
 
     if (!isMemberInQueue(member)) {
         queue.push(member);
-        textChannel.send(`${member.displayName} entrou na fila.`);
+        textChannel.send(`:white_check_mark: **${member.displayName} entrou na fila.**`);
     }
     else {
-        textChannel.send(`${member.displayName} já está na fila.`);
+        textChannel.send(`**${member.displayName} já está na fila.** :neutral_face:`);
     }
 }
 
 function leaveQueue(member, textChannel) {
     if (isMemberInQueue(member)) {
         remove(member);
-        textChannel.send(`${member.displayName} saiu da fila.`);
+        textChannel.send(`:heavy_multiplication_x: **${member.displayName} saiu da fila.**`);
     }
     else {
-        textChannel.send(`${member.displayName} não está na fila.`);
+        textChannel.send(`**${member.displayName} não está na fila.** :thinking:`);
     }
 }
 
@@ -85,34 +83,49 @@ function remove(member) {
     return removed;
 }
 
-function queueNext(textChannel) {
+function queueNext(member, textChannel) {
+
+    if (!member.hasPermission('MOVE_MEMBERS'))
+        return;
 
     if (!waitChannel || !playChannel) {
-        textChannel.send('É necessário configurar os canais de jogo e espera.')
+        textChannel.send('**É necessário configurar os canais de jogo e espera.** :face_palm:')
         return;
     }
 
     if (active) {
         active.setVoiceChannel(waitChannel)
-            .then(() => textChannel.send(`${active.displayName} => ${playChannel.name}`));
+            .then(() => textChannel.send(`${active.displayName} :arrow_right: ${playChannel.name}`));
         active = null;
     }
 
     if (queue.length > 0)
         active = queue.shift();
     else
-        textChannel.send('A fila está vazia.')
+        textChannel.send('**A fila está vazia. :cry:**')
 
     if (active)
         active.setVoiceChannel(playChannel)
-            .then(() => textChannel.send(`${active.displayName} => ${playChannel.name}`));
+            .then(() => textChannel.send(`${active.displayName} :arrow_right: ${playChannel.name}`));
 }
 
 function setChannel(member, textChannel) {
     playChannel = member.voiceChannel;
-    textChannel.send(`Canal de jogo: ${playChannel.name}`)
+    textChannel.send(`**Canal de jogo:** ${playChannel.name}`)
 }
 function setWaitChannel(member, textChannel) {
     waitChannel = member.voiceChannel;
-    textChannel.send(`Canal de espera: ${waitChannel.name}`)
+    textChannel.send(`**Canal de espera:** ${waitChannel.name}`)
+}
+
+function listQueue(textChannel) {
+    var ret = "**Fila:** ```";
+
+    for(i = 0; i < queue.length; i++)
+    {
+        ret += `\n${queue[i].displayName}`;
+    }
+    ret += "```";
+
+    textChannel.send(ret);
 }
