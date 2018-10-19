6721367d13c82035b11a592f50b7f5eb4d06cf4a
Gui Rangel <guirangel.310@gmail.com>
2018-10-19 15:01:09 -0300

    adding structure for custom command

--- src/modules/event-handler.module.js
+++ src/modules/event-handler.module.js
@@ -1,8 +1,51 @@
 const logger = require('../logger.js');
 
 
-
 class EventHandler {
+
+    init(bot) {
+        for (var key in this.events) {
+            var event = events[key];
+            bot.client.on(key, event)
+        }
+    }
+
+    events = {
+        message: msg => {
+            if (msg.content && this.isCommand(msg.content, bot.config.command.symbol)) {
+
+                if (msg.author.id != bot.client.user.id) {
+                    logger.log(msg.guild ? msg.guild.name + ' ' : '' + msg.channel.name ? '#' + msg.channel.name + ' @' : 'PM @' + msg.author.username + ': ' + msg.content);
+                }
+
+                var data = this.parseMsg(msg, bot.config.command.symbol);
+
+                bot.executeCommand(data);
+
+            }
+        },
+
+        ready: () => {
+            if (bot.online)
+                logger.log('Reconnected.');
+            else
+                logger.log('SupBot Online.');
+            bot.online = true;
+        },
+
+        reconnecting: () => {
+            logger.log('Reconnecting...');
+        },
+
+        disconnect: () => {
+            bot.online = false;
+            logger.log('Disconnected.');
+        },
+
+        error: error => {
+            logger.error(error);
+        }
+    }
 
     parseMsg(msg, symbol) {
         msg.meta = msg.content.split(' ');
@@ -16,58 +59,21 @@ class EventHandler {
         return content.substring(0, symbol.length) == symbol;
     }
 
-    init(bot) {
-        let events = {
-            message: msg => {
-                if (msg.content && this.isCommand(msg.content, bot.config.command.symbol)) {
-
-                    if (msg.author.id != bot.client.user.id) {
-                        logger.log(
-                            msg.guild ? msg.guild.name + ' ' : '' +
-                                msg.channel.name ? '#' + msg.channel.name + ' @' : 'PM @' +
-                                msg.author.username +
-                                ': ' + msg.content);
-                    }
-
-                    try {
-                        var data = this.parseMsg(msg, bot.config.command.symbol);
-                        var cmd = bot.commands[data.cmd];
-                        if (typeof cmd === "function") {
-                            cmd(data);
-                        }
-                        
-                    } catch (e) {
-                        logger.error(e);
-                    }
-                }
-            },
-
-            ready: () => {
-                if (bot.online)
-                    logger.log('Reconnected.');
-                else
-                    logger.log('SupBot Online.');
-                bot.online = true;
-            },
-
-            reconnecting: () => {
-                logger.log('Reconnecting...');
-            },
-
-            disconnect: () => {
-                bot.online = false;
-                logger.log('Disconnected.');
-            },
-
-            error: error => {
-                logger.error(error);
+    executeCommand(data, bot) {
+        const cmd = bot.commands[data.msg]
+        if (cmd) {
+            if (typeof cmd === "function") {
+                cmd(data);
             }
         }
-
-        for (var key in events) {
-            var event = events[key];
-            bot.client.on(key, event)
+        else {
+            this.executeCustom(data, bot);
         }
+    }
+
+    executeCustom(data, bot) {
+        console.log(data.msg);
+        console.log(data.details);
     }
 }
 
